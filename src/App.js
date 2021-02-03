cf105f898f737fa3e894655da8a17cbbb7f4fb36
Justas Titovec <justas.titovec@yahoo.com>
2021-02-03 13:36:06 +0100

    infinte scroll congif

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GifsList from './components/GifsList';
 import SearchBar from './components/SearchBar';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 import giphy from './apis/giphy';
 
@@ -9,6 +9,9 @@ const KEY = 'og6TjIlZV6HMhIFiXNXHFxJySKhD0aeu';
 
 function App() {
   const [gifs, setGifs] = useState([]);
+  const [term, setTerm] = useState('');
+
+  let count = 0;
 
   const onTermSubmit = async (term) => {
     const response = await giphy.get('/gifs/search', {
@@ -22,10 +25,32 @@ function App() {
     setGifs(response.data.data);
   };
 
+  const fetchData = async (term) => {
+    const response = await giphy.get('/gifs/search', {
+      params: {
+        api_key: KEY,
+        limit: 24,
+        q: term,
+        offset: 13,
+      },
+    });
+
+    setGifs([...gifs, ...response.data.data]);
+    console.log(gifs.length);
+  };
+
   return (
     <div className="app">
       <SearchBar onFormSubmit={onTermSubmit} />
-      <GifsList gifs={gifs} />
+
+      <InfiniteScroll
+        dataLength={gifs.length}
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <GifsList gifs={gifs} />
+      </InfiniteScroll>
     </div>
   );
 }
